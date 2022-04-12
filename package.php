b277c8723a07d1b293383308f1f5a6c388d9ab80
Rana12356 <rana15-12356@diu.edu.bd>
2022-04-12 13:18:17 +0600

    full project done

---
+++ package.php
@@ -0,0 +1,223 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>package</title>
+
+   <!-- swiper css link  -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+
+   <!-- header section starts  -->
+
+   <section class="header">
+      <a href="home.php" class="logo">Traveller</a>
+      <nav class="navbar">
+         <a href="home.php">home</a>
+         <a href="about.php">about</a>
+         <a href="package.php">package</a>
+         <a href="book.php">book</a>
+      </nav>
+      <div id="menu-btn" class="fas fa-bars"></div>
+   </section>
+
+   <!-- header section ends -->
+   <div class="heading" style="background:url(images/header-bg-2.png) no-repeat">
+      <h1>packages</h1>
+   </div>
+   <!-- packages section starts  -->
+   <section class="packages">
+      <h1 class="heading-title">top destinations</h1>
+      <div class="box-container">
+         <div class="box">
+            <div class="image">
+               <img src="images/Dhaka.jpg" alt="">
+            </div>
+            <div class="content">
+               <h3>Dhaka</h3>
+               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, perspiciatis!</p>
+               <a href="book.php" class="btn">book now</a>
+            </div>
+         </div>
+         <div class="box">
+            <div class="image">
+               <img src="images/chatto.jpg" alt="">
+            </div>
+            <div class="content">
+               <h3>Chattogram</h3>
+               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, perspiciatis!</p>
+               <a href="book.php" class="btn">book now</a>
+            </div>
+         </div>
+         <div class="box">
+            <div class="image">
+               <img src="images/bagherhat.jpg" alt="">
+            </div>
+            <div class="content">
+               <h3>Bagherhat</h3>
+               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, perspiciatis!</p>
+               <a href="book.php" class="btn">book now</a>
+            </div>
+         </div>
+
+         <div class="box">
+            <div class="image">
+               <img src="images/bandarban.jpg" alt="">
+            </div>
+            <div class="content">
+               <h3>Bandarban</h3>
+               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, perspiciatis!</p>
+               <a href="book.php" class="btn">book now</a>
+            </div>
+         </div>
+         <div class="box">
+            <div class="image">
+               <img src="images/sylhet.jpg" alt="">
+            </div>
+            <div class="content">
+               <h3>Sylhet</h3>
+               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, perspiciatis!</p>
+               <a href="book.php" class="btn">book now</a>
+            </div>
+         </div>
+         <div class="box">
+            <div class="image">
+               <img src="images/rangamati.jpg" alt="">
+            </div>
+            <div class="content">
+               <h3>Rangamati</h3>
+               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, perspiciatis!</p>
+               <a href="book.php" class="btn">book now</a>
+            </div>
+         </div>
+         <div class="box">
+            <div class="image">
+               <img src="images/tangail.jpg" alt="">
+            </div>
+            <div class="content">
+               <h3>Tangail</h3>
+               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, perspiciatis!</p>
+               <a href="book.php" class="btn">book now</a>
+            </div>
+         </div>
+         <div class="box">
+            <div class="image">
+               <img src="images/jassore.jpg" alt="">
+            </div>
+            <div class="content">
+               <h3>Jassore</h3>
+               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, perspiciatis!</p>
+               <a href="book.php" class="btn">book now</a>
+            </div>
+         </div>
+         <div class="box">
+            <div class="image">
+               <img src="images/Noakhali.jpg" alt="">
+            </div>
+            <div class="content">
+               <h3>Noakhali</h3>
+               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, perspiciatis!</p>
+               <a href="book.php" class="btn">book now</a>
+            </div>
+         </div>
+         <div class="box">
+            <div class="image">
+               <img src="images/cox.jpg" alt="">
+            </div>
+            <div class="content">
+               <h3>Cox's Bazar</h3>
+               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, perspiciatis!</p>
+               <a href="book.php" class="btn">book now</a>
+            </div>
+         </div>
+         <div class="box">
+            <div class="image">
+               <img src="images/dinajpur.jpg" alt="">
+            </div>
+            <div class="content">
+               <h3>Dinajpur</h3>
+               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, perspiciatis!</p>
+               <a href="book.php" class="btn">book now</a>
+            </div>
+         </div>
+         <div class="box">
+            <div class="image">
+               <img src="images/bogura.jpg" alt="">
+            </div>
+            <div class="content">
+               <h3>Bogura</h3>
+               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, perspiciatis!</p>
+               <a href="book.php" class="btn">book now</a>
+            </div>
+         </div>
+      </div>
+      <div class="load-more"><span class="btn">load more</span></div>
+   </section>
+   <!-- packages section ends -->
+
+   <!-- footer section starts  -->
+
+   <section class="footer">
+
+      <div class="box-container">
+
+         <div class="box">
+            <h3>quick links</h3>
+            <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+            <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+            <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+            <a href="book.php"> <i class="fas fa-angle-right"></i> book</a>
+         </div>
+
+         <div class="box">
+            <h3>contact info</h3>
+            <a href="#"> <i class="fas fa-phone"></i> 01623905299 </a>
+            <a href="#"> <i class="fas fa-phone"></i> 01315831826 </a>
+            <a href="#"> <i class="fas fa-envelope"></i>teamanamalous@diu.edu.bd </a>
+            <a href="#"> <i class="fas fa-map"></i> Tangail, Dhaka, Bangladesh </a>
+         </div>
+
+         <div class="box">
+            <h3>follow us</h3>
+            <a target="_blank" href="https://m.me/ranaahmed12356"> <i class="fab fa-facebook-f"></i> facebook </a>
+            <a target="_blank" href="https://m.me/ranaahmed12356"> <i class="fab fa-twitter"></i> twitter </a>
+            <a target="_blank" href="#"> <i class="fab fa-instagram"></i> instagram </a>
+            <a target="_blank" href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+         </div>
+
+      </div>
+      <div class="credit"> created by <span>team anamalous</span> | all rights reserved! </div>
+
+   </section>
+
+   <!-- footer section ends -->
+
+
+
+
+
+
+
+
+
+   <!-- swiper js link  -->
+   <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+   <!-- custom js file link  -->
+   <script src="js/script.js"></script>
+
+</body>
+
+</html>
